aebdf52e5abff13be6a91ad55acb879e53ea4a7a
Josip Frljic <josipfrljic77@gmail.com>
2020-10-17 00:04:44 +0200

    making db/ proclife

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const pagesRouter=require('./routes/topics');
 const Topic= require('./models/topic')
 const ejs = require('ejs');
 const mongoose=require('mongoose');
+const connectToDB=require("./config/connectToDB");
 const methodOverride= require('method-override')
 const slugify= require('slugify');
 const bodyParser = require('body-parser');
@@ -14,10 +15,11 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+connectToDB();
 
-mongoose.connect('mongodb://localhost/personalblog',{
-    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
-})
+// mongoose.connect(CONNECTION_URI,{
+//     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true
+// })
 app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended:false}));
 app.use(methodOverride('_method'));
